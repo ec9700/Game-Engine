72c68fb21a874f6da65082b611e888acdd87e1e4
Carson King <ck6100@students.inghamisd.org>
2024-03-20 11:32:25 -0400

    teehee

---
+++ cmake-build-debug/CMakeFiles/GameLib.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ck6100/Documents/Game-Engine/glad.c" "CMakeFiles/GameLib.dir/glad.c.obj" "gcc" "CMakeFiles/GameLib.dir/glad.c.obj.d"
+  "C:/Users/ck6100/Documents/Game-Engine/gameEngine/Component.cpp" "CMakeFiles/GameLib.dir/gameEngine/Component.cpp.obj" "gcc" "CMakeFiles/GameLib.dir/gameEngine/Component.cpp.obj.d"
+  "C:/Users/ck6100/Documents/Game-Engine/gameEngine/Entity.cpp" "CMakeFiles/GameLib.dir/gameEngine/Entity.cpp.obj" "gcc" "CMakeFiles/GameLib.dir/gameEngine/Entity.cpp.obj.d"
+  "C:/Users/ck6100/Documents/Game-Engine/gameEngine/GameManager.cpp" "CMakeFiles/GameLib.dir/gameEngine/GameManager.cpp.obj" "gcc" "CMakeFiles/GameLib.dir/gameEngine/GameManager.cpp.obj.d"
+  "C:/Users/ck6100/Documents/Game-Engine/gameEngine/RenderManager.cpp" "CMakeFiles/GameLib.dir/gameEngine/RenderManager.cpp.obj" "gcc" "CMakeFiles/GameLib.dir/gameEngine/RenderManager.cpp.obj.d"
+  "C:/Users/ck6100/Documents/Game-Engine/gameEngine/Shader.cpp" "CMakeFiles/GameLib.dir/gameEngine/Shader.cpp.obj" "gcc" "CMakeFiles/GameLib.dir/gameEngine/Shader.cpp.obj.d"
+  "C:/Users/ck6100/Documents/Game-Engine/gameEngine/ShaderCollection.cpp" "CMakeFiles/GameLib.dir/gameEngine/ShaderCollection.cpp.obj" "gcc" "CMakeFiles/GameLib.dir/gameEngine/ShaderCollection.cpp.obj.d"
+  "C:/Users/ck6100/Documents/Game-Engine/gameEngine/Texture.cpp" "CMakeFiles/GameLib.dir/gameEngine/Texture.cpp.obj" "gcc" "CMakeFiles/GameLib.dir/gameEngine/Texture.cpp.obj.d"
+  "C:/Users/ck6100/Documents/Game-Engine/main.cpp" "CMakeFiles/GameLib.dir/main.cpp.obj" "gcc" "CMakeFiles/GameLib.dir/main.cpp.obj.d"
+  "C:/Users/ck6100/Documents/Game-Engine/stb_image.cpp" "CMakeFiles/GameLib.dir/stb_image.cpp.obj" "gcc" "CMakeFiles/GameLib.dir/stb_image.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/GameLib.dir/gameEngine/Component.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/GameLib.dir/gameEngine/Component.cpp.obj: \
+ C:\Users\ck6100\Documents\Game-Engine\gameEngine\Component.cpp \
+ C:\Users\ck6100\Documents\Game-Engine\gameEngine\Component.h
