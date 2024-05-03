60784f699b80d8a426e00e56a760380926655eb2
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-05-03 09:39:17 -0400

    no build s

--- cmake-build-debug/CMakeFiles/GameLib.dir/gameEngine/GameManager.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/GameLib.dir/gameEngine/GameManager.cpp.obj: \
- C:\Users\carson\Documents\Game-Engine\gameEngine\GameManager.cpp \
- C:\Users\carson\Documents\Game-Engine\gameEngine\GameManager.h
--- cmake-build-debug/CMakeFiles/GameLib.dir/gameEngine/vector2.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/GameLib.dir/gameEngine/vector2.cpp.obj: \
- C:\Users\carson\Documents\Game-Engine\gameEngine\vector2.cpp \
- C:\Users\carson\Documents\Game-Engine\gameEngine\vector2.h
